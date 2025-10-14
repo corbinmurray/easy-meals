1b496f6fca1cf00c814cdcc479f5fd96cdacb86f
corbin murray <corbin.murray63@yahoo.com>
2025-10-14 15:51:46 -0500

    feat: implement RecipeEngine service registration and logging in Program.cs

---
+++ apps/recipe-engine/src/EasyMeals.RecipeEngine.Application/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using EasyMeals.RecipeEngine.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyMeals.RecipeEngine.Application.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddRecipeEngine(this IServiceCollection services)
+	{
+		services.AddTransient<IRecipeEngine, Services.RecipeEngine>();
+
+		return services;
+	}
+}
--- apps/recipe-engine/src/EasyMeals.RecipeEngine/Program.cs
+++ apps/recipe-engine/src/EasyMeals.RecipeEngine/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using EasyMeals.RecipeEngine.Application.DependencyInjection;
+using EasyMeals.RecipeEngine.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var services = new ServiceCollection();
+
+services.AddLogging(opts =>
+{
+	opts.AddConsole();
+	opts.AddDebug();
+});
+
+services.AddRecipeEngine();
+
+await using ServiceProvider serviceProvider = services.BuildServiceProvider(true);
+
+var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+var recipeEngine = serviceProvider.GetRequiredService<IRecipeEngine>();
+
+logger.LogInformation("Starting Recipe Engine...");
+
+await recipeEngine.RunAsync();
