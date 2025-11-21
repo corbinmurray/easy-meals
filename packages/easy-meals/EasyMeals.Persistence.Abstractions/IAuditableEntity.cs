42b9d317e77c11459e0b6755b660337452650253
corbin murray <corbin.murray63@yahoo.com>
2025-11-20 20:15:55 -0600

    feat: add initial project structure and interfaces for persistence and platform layers

---
+++ packages/easy-meals/EasyMeals.Persistence.Abstractions/IAuditableEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace EasyMeals.Persistence.Abstractions;
+
+/// <summary>
+///  Represents an entity that tracks creation and modification timestamps.
+/// </summary>
+public interface IAuditableEntity
+{
+	/// <summary>
+	///  Gets or sets the date and time when the entity was created.
+	/// </summary>
+	DateTime CreatedAt { get; set; }
+
+	/// <summary>
+	///  Gets or sets the date and time when the entity was last updated.
+	/// </summary>
+	DateTime UpdatedAt { get; set; }
+
+	/// <summary>
+	///  Marks the entity as modified, updating the UpdatedAt timestamp.
+	/// </summary>
+	void MarkAsModified();
+}
---
+++ packages/easy-meals/EasyMeals.Persistence.Abstractions/IEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyMeals.Persistence.Abstractions;
+
+/// <summary>
+/// Represents a basic entity.
+/// </summary>
+public interface IEntity
+{
+	/// <summary>
+    /// Gets or sets the unique identifier of the entity.
+    /// </summary>
+	string Id { get; set; }
+
+	
+}
